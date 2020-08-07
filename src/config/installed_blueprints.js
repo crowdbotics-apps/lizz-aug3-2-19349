302b0fb73c2e9847bc40b578146044b912f596d9
Crowdbotics <team@crowdbotics.com>
2020-08-07 17:46:51 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen588737', human_name: 'Blank Screen 5', access_route: 'BlankScreen588737'},
 { name: 'BlankScreen488736', human_name: 'Blank Screen 4', access_route: 'BlankScreen488736'},
 { name: 'BlankScreen388735', human_name: 'Blank Screen 3', access_route: 'BlankScreen388735'},
 { name: 'BlankScreen187120', human_name: 'Blank Screen 1', access_route: 'BlankScreen187120'},
